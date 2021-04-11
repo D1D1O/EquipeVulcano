6dd2f68eeec576db5bf8b6a286b7787adb5782c3
Giovanny Lima da Silva <didiosilva395@gmail.com>
2021-04-11 14:34:49 -0400

    Dia 2 aula 3

--- app/src/main/java/com/hefesto/pokedex_equipevulcano/MainActivity.kt
+++ app/src/main/java/com/hefesto/pokedex_equipevulcano/MainActivity.kt
@@ -2,10 +2,17 @@ package com.hefesto.pokedex_equipevulcano
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        shouldDisplayEmptyView(true)
+    }
+
+    fun shouldDisplayEmptyView(isEmpty: Boolean) {
+        emptyView.visibility = if (isEmpty) View.VISIBLE else View.GONE
     }
 }
