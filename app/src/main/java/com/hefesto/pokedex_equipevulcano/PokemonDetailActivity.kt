ab880aaa879ea3c66f1d6fd0b147dcab273f60e9
Giovanny Lima da Silva <didiosilva395@gmail.com>
2021-04-12 19:19:07 -0400

    Funcional

--- app/src/main/java/com/hefesto/pokedex_equipevulcano/PokemonDetailActivity.kt
+++ app/src/main/java/com/hefesto/pokedex_equipevulcano/PokemonDetailActivity.kt
@@ -5,25 +5,50 @@ import android.os.Bundle
 import android.view.View
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_pokemon_detail.*
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
 class PokemonDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pokemon_detail)
-        intent.getParcelableExtra<Pokemon>(POKEMON_EXTRA)?.let{
-            tvName.text = it.name
-            tvNumber.text = "#%03d".format(it.number)
-            tvFirstType.text = it.types.getOrNull(0)
-            val secondType:String? = it.types.getOrNull(1)
+        intent.getParcelableExtra<Pokemon>(POKEMON_EXTRA)?.let{pokemon ->
+            tvName.text = pokemon.name
+            tvNumber.text = "#%03d".format(pokemon.number)
+            tvFirstType.text = pokemon.types.getOrNull(0)
+            val secondType:String? = pokemon.types.getOrNull(1)
             if(secondType == null){
                 tvSecondType.visibility = View.GONE
             }else{
                 tvSecondType.visibility = View.VISIBLE
                 tvSecondType.text = secondType
             }
-            tvWeight.text = "%.1f kg".format(it.weight)
-            tvHeight.text = "%.2f cm".format(it.height/10)
-            Picasso.get().load(it.imageUrl).into(ivImage)
+            tvWeight.text = "%.1f kg".format(pokemon.weight)
+            tvHeight.text = "%.2f cm".format(pokemon.height/10)
+            Picasso.get().load(pokemon.imageUrl).into(ivImage)
+
+            val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+            mapFragment.getMapAsync {
+                it.uiSettings.isZoomControlsEnabled = true
+                val latLng = LatLng(pokemon.latitude,pokemon.longitude)
+
+
+                val marker = MarkerOptions().position(latLng).icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_marker))
+                it.addMarker(marker)
+
+
+
+
+
+                it.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+                it.moveCamera(CameraUpdateFactory.zoomTo(15f))
+
+            }
         }
     }
 
